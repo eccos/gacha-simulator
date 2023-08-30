03d4eca677ae5b20248b9f73bc4ea12c393b0326
eccos <eccos@users.noreply.github.com>
2023-08-30 15:20:08 -0700

    add initial html and empty supporting files

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * gacha simulator
+ * chance = 1 - (1 - p)^n
+ * p = trail percentage
+ * n = number of trials
+ * conf
+ * i = store every i step(s) in array
+ * d = decimal place to round
+ */
+
