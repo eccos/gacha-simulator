57d8975628d3147790c2d845df4a11f494eac92c
eccos <eccos@users.noreply.github.com>
2023-08-30 17:24:50 -0700

    create tbl from num of trials with % chance

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ const inpIds = ['#percentChance', '#numTrial', '#step', '#fixedDec'];
 /** @type {HTMLInputElement[]} */
 const inps = inpIds.map(id => document.querySelector(id));
 const [inpPercentChance, inpNumTrial, inpStep, inpFixedDec] = inps;
+/** @type {HTMLDivElement} */
+const elResult = document.querySelector("#result");
+
 inps.forEach((inp) => {
     inp.addEventListener("change", updateDOM);
 });
-/** @type {HTMLDivElement} */
-const elResult = document.querySelector("#result");
 
 updateDOM();
 
@@ -25,15 +26,49 @@ function updateDOM() {
     const n = Number(inpNumTrial.value);
 
     const trialPercents = [];
-    for (let i = 1; i < n; i++) {
-        trialPercents.push(calcChance(p, i));
+    for (let i = 1; i <= n; i++) {
+        trialPercents.push([i, calcChance(p, i)]);
     }
-    elResult.textContent = calcChance(p, n);
+    const thVals = ["Trial", "% Chance"];
+    const tbl = createTable(thVals, trialPercents);
+    elResult.innerHTML = null;
+    elResult.appendChild(tbl);
+}
+
+/**
+ * Creates a table from the passed in data
+ * @param {[]} [thVals] array of values to use as table headers
+ * @param {[[]]} [trRows] array of arrays to use as table rows
+ * @returns {HTMLTableElement}
+ */
+function createTable(thVals, trRows) {
+    const tbl = document.createElement("table");
+    if (thVals && thVals.length > 0) {
+        const thead = tbl.createTHead();
+        const thRow = thead.insertRow();
+        thVals.forEach(thVal => {
+            const thCell = thRow.appendChild(document.createElement("th"));
+            thCell.textContent = thVal;
+        });
+    }
+    if (trRows && trRows.length > 0) {
+        const tbody = tbl.createTBody();
+        trRows.forEach(trRow => {
+            const row = tbody.insertRow();
+            trRow.forEach(rowVal => {
+                // const cell = row.insertCell();
+                // cell.textContent = i + 1;
+                const cell = row.insertCell();
+                cell.textContent = rowVal;
+            })
+        });
+    }
+    return tbl;
 }
 
 function calcChance(trialPercentage, numOfTrials) {
     const [p, n] = [trialPercentage, numOfTrials];
     if (!p || !n) return;
-    const chance = (1 - Math.pow((1 - p/100), n)) * 100;
+    const chance = (1 - Math.pow((1 - p / 100), n)) * 100;
     return chance;
 }
